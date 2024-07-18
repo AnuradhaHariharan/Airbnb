988c63d583a036d9d95816ae655e89393a48f35a
anuradha <anuradhahariharannov5@gmail.com>
2024-07-18 20:03:23 +0530

    db

--- init/index.js
+++ init/index.js
@@ -2,7 +2,9 @@ const mongoose=require("mongoose");
 const data=require("./data");
 const Listing=require("../models/listing.js");
 
-
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  }
   main().then(()=>console.log("Successfull")).catch(err => console.log(err));
 
   const initDb=async ()=>{
